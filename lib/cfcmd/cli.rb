bbf739248419d9bc141c2b26c24f96d92be77537
Adam Lassek <adam@doubleprime.net>
2015-09-07 19:35:54 -0500

    Interactive configuration, friendlier failures

--- lib/cfcmd/cli.rb
+++ lib/cfcmd/cli.rb
@@ -11,6 +11,8 @@ module CFcmd
     no_commands do
       def config
         @config ||= TOML.load_file(File.expand_path(ENV['HOME'] + '/.cfcmd'))
+      rescue Errno::ENOENT
+        abort "Config file not found. You should run 'cfcmd configure' first."
       end
 
       def region
@@ -24,7 +26,41 @@ module CFcmd
           rackspace_api_key:  config.fetch('rackspace', {}).fetch('api_key'),
           rackspace_region:   region.downcase.to_sym
         })
+      rescue Excon::Errors::Unauthorized
+        abort "Username or api key is invalid."
+      rescue Excon::Errors::SocketError
+        abort "Unable to connect to Rackspace."
       end
+    end
+
+    desc "configure", "Invoke interactive (re)configuration tool."
+    def configure
+      config_file = File.expand_path(ENV['HOME'] + '/.cfcmd')
+      config      = { rackspace: {} }
+
+      print "Rackspace Username: "
+      config[:rackspace][:username] = STDIN.gets.chomp
+      print "Rackspace API key: "
+      config[:rackspace][:api_key] = STDIN.gets.chomp
+      print "Default Region [ord]: "
+      region = STDIN.gets.chomp
+      region = 'ord' unless region.length > 0
+      config[:rackspace][:region] = region
+
+      File.open config_file, 'w' do |file|
+        file.write TOML::Generator.new(config).body
+      end
+
+      puts "Wrote configuration to #{ config_file }"
+    rescue Interrupt
+      print "\n"
+    end
+
+    map '--configure' => 'configure'
+
+    desc 'test', 'test'
+    def test
+      connection.account
     end
 
     desc "ls [cf://BUCKET[/PREFIX]] [options]", "List objects or buckets"
